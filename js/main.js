128c159255c31e12dde1d83cfe2850e436f29d14
anas374 <93293678+Anas374@users.noreply.github.com>
2025-03-17 01:50:44 +0700

    -add hover trailing ink effect -add touchscreen-like mouse event behaviour for drawing -prevent dragging behaviour for all div inside the canvas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector(".canvas");
+let painting = false;
 
 function gridBuilder (gridSize) {
     for (let i = 0; i < gridSize; i++) {
@@ -6,14 +7,18 @@ function gridBuilder (gridSize) {
         gridRow.style.display = "flex";
         gridRow.style.flexDirection = "column";
         gridRow.style.flexGrow = "1";
-        gridRow.style.border = "1px solid black";
+        gridRow.style.borderLeft = "1px solid black";
+        gridRow.style.borderRight = "1px solid black";
         gridRow.style.borderCollapse = "collapse";
         canvas.appendChild(gridRow);
         for (let j = 0; j < gridSize; j++) {
             const gridColumn = document.createElement("div");
             gridColumn.style.flexGrow = "1";
-            gridColumn.style.border = "1px solid black";
+            gridColumn.style.borderTop = "1px solid black";
+            gridColumn.style.borderBottom = "1px solid black";
             gridColumn.style.borderCollapse = "collapse";
+            gridColumn.style.backgroundColor = "white";
+            gridColumn.setAttribute("draggable", "false");
             gridRow.appendChild(gridColumn);
         }
     } 
@@ -26,6 +31,25 @@ const grid = document.querySelector(".canvas").querySelectorAll("div");
 grid.forEach(column => {
     const gridBlocks = column.querySelectorAll("div");
     gridBlocks.forEach(gridBlock => {
-        gridBlock.addEventListener("mouseover", () => {gridBlock.style.backgroundColor = "black"});
+        gridBlock.addEventListener("mouseover", () => {
+            setTimeout(() => {
+                if (gridBlock.style.backgroundColor !== "white") return;
+                gridBlock.style.backgroundColor = "black";
+                setTimeout(() => {
+                    gridBlock.style.backgroundColor = "white";
+                }, 300);
+            }, 50);
+        });
+
+        gridBlock.addEventListener("mousedown", (e) => {
+            e.preventDefault();
+            painting = true;
+        });
+        gridBlock.addEventListener("mouseup", () => {painting = false});
+
+        gridBlock.addEventListener("mousemove", () => {
+            if (!painting) return;
+            gridBlock.style.backgroundColor = "black";
+        });
     })
 })
