8834115a9de728b717524a9728aa77dd4ecd8fa6
anas374 <93293678+Anas374@users.noreply.github.com>
2025-03-17 15:55:44 +0700

    Completed the features -add pick color feature -add randomize color feature -add change grid size feature -add clear canvas feature

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const canvas = document.querySelector(".canvas");
 let painting = false;
+let randomizeColor = false;
 
 function gridBuilder (gridSize) {
+    destroy();
+    gridSize = parseInt(gridSize);
+
     for (let i = 0; i < gridSize; i++) {
         const gridRow = document.createElement("div");
         gridRow.style.display = "flex";
@@ -21,35 +25,77 @@ function gridBuilder (gridSize) {
             gridColumn.setAttribute("draggable", "false");
             gridRow.appendChild(gridColumn);
         }
-    } 
+    }
+    
+    const grid = document.querySelector(".canvas").querySelectorAll("div");
+    grid.forEach(column => {
+        const gridBlocks = column.querySelectorAll("div");
+        gridBlocks.forEach(gridBlock => {
+            gridBlock.addEventListener("mouseover", () => {
+                setTimeout(() => {
+                    if (gridBlock.style.backgroundColor !== "white") return;
+                    if (randomizeColor) {
+                        color = getRandomHexColor();
+                    }
+                    gridBlock.style.backgroundColor = color;
+                    setTimeout(() => {
+                        gridBlock.style.backgroundColor = "white";
+                    }, 300);
+                }, 50);
+            });
+
+            gridBlock.addEventListener("mousedown", (e) => {
+                e.preventDefault();
+                painting = true;
+            });
+            gridBlock.addEventListener("mouseup", () => {painting = false});
+
+            gridBlock.addEventListener("mousemove", () => {
+                if (!painting) return;
+                if (randomizeColor) {
+                    color = getRandomHexColor();
+                    gridBlock.style.backgroundColor = color;
+                }
+                gridBlock.style.backgroundColor = color;
+            });
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', gridBuilder(16));
 
-const grid = document.querySelector(".canvas").querySelectorAll("div");
+const colorInput = document.querySelector("#color");
+let color = "#000000";
+colorInput.addEventListener('input', () => {color = colorInput.value});
 
-grid.forEach(column => {
-    const gridBlocks = column.querySelectorAll("div");
-    gridBlocks.forEach(gridBlock => {
-        gridBlock.addEventListener("mouseover", () => {
-            setTimeout(() => {
-                if (gridBlock.style.backgroundColor !== "white") return;
-                gridBlock.style.backgroundColor = "black";
-                setTimeout(() => {
-                    gridBlock.style.backgroundColor = "white";
-                }, 300);
-            }, 50);
-        });
+const changeSize = document.querySelector("#change-size");
+changeSize.addEventListener('click', () => {gridBuilder(prompt("Ukuran Grid (Max 100)"))});
 
-        gridBlock.addEventListener("mousedown", (e) => {
-            e.preventDefault();
-            painting = true;
-        });
-        gridBlock.addEventListener("mouseup", () => {painting = false});
+function destroy() {
+    const blocks = canvas.querySelectorAll("div");
+    blocks.forEach(block => block.remove());
+}
 
-        gridBlock.addEventListener("mousemove", () => {
-            if (!painting) return;
-            gridBlock.style.backgroundColor = "black";
-        });
+const clearCanvas = document.querySelector("#clear-canvas");
+clearCanvas.addEventListener('click', () => {
+    const blocks = canvas.querySelectorAll("div");
+    blocks.forEach((block) => {
+        block.style.backgroundColor = "white";
     })
-})
+});
+
+function getRandomHexColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
+
+const randomColorBtn = document.querySelector("#random-color");
+randomColorBtn.addEventListener('click', () => {
+    if(!randomizeColor) {
+        randomizeColor = true;
+        randomColorBtn.style.backgroundColor = "lightgrey";
+    } else {
+        randomizeColor = false;
+        randomColorBtn.style.backgroundColor = "white";
+        color = colorInput.value;
+    }
+});
